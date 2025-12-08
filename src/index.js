b74b6c9df27c62a838a2c2509098568592ce726b
EL MOSTAFA BELAYD <2007013000103@ofppt-edu.ma>
2025-12-08 12:35:52 +0100

    new v

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
-import List from "./testRdxThnk/List";
 import App from "./testRdxThnk/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
