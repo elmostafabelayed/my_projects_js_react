7da5539c24d8f66da1f6ba8000de977f60838843
EL MOSTAFA BELAYD <2007013000103@ofppt-edu.ma>
2026-01-24 16:32:44 +0100

    odifi

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import ReactDOM from "react-dom/client";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
-import App from "./corr_TP7_RTK/Composants/App";
+// import App from "./corr_TP7_RTK/Composants/App";
+import App from "./notes_app/App"
 import Navbar from "./Thunk_Tp/composon/Navbar";
+import { Provider } from "react-redux";
+import sotre from "./notes_app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <Provider store={sotre}>
+<BrowserRouter>
     <App />
   </BrowserRouter>
+  </Provider>
+  
 );
